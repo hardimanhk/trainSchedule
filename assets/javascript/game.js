1c79ea408da6211947d40bcd84e666b4bee93af9
hilbo <hardimanhk@gmail.com>
2018-09-18 16:56:50 -0400

    added functionality to update the page every minute

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,9 +18,10 @@ var firstTime = "";
 var intervalTime = "";
 var nextTrain = "";
 var minutesAway = "";
+var rowCount = 0;
 console.log("anything");
 
-$("#submit").click(function(event) {
+$("#submit").click(function (event) {
     event.preventDefault();
 
     trainName = $("#train-name").val().trim();
@@ -42,9 +43,11 @@ $("#submit").click(function(event) {
     database.ref().push({
         trainName: trainName,
         destination: destination,
+        firstTime: firstTime,
         frequency: intervalTime,
         nextTrain: nextTrain.format("hh:mm"),
         minutesAway: minutesAway.toString(),
+        row: rowCount
     });
 
     $("#train-name").val("");
@@ -53,7 +56,7 @@ $("#submit").click(function(event) {
     $("#frequency").val("");
 });
 
-database.ref().on("child_added", function(snapshot){
+database.ref().on("child_added", function (snapshot) {
     var thisTrainName = snapshot.val().trainName;
     var thisDestination = snapshot.val().destination;
     var thisFrequency = snapshot.val().frequency;
@@ -64,10 +67,47 @@ database.ref().on("child_added", function(snapshot){
         $("<td>").text(thisTrainName),
         $("<td>").text(thisDestination),
         $("<td>").text(thisFrequency),
-        $("<td>").text(thisUpcomingTime),
-        $("<td>").text(thisMinutesAway)
+        $(`<td id='next-train${rowCount}'>`).text(thisUpcomingTime),
+        $(`<td id='minutes-away${rowCount}'>`).text(thisMinutesAway)
     )
-    
+    newRow.addClass("table-row");
+    newRow.attr("row-number", rowCount);
+
     $("#train-table-body").prepend(newRow);
+    rowCount++;
 
 });
+
+// to update each minute: loop through each existing row and update to current time
+// need data from the database
+// need to access each row's 4th and 5th elements
+// use a value change, change the data in the database and use that to trigger the update to the page
+// this was a fucking challenge :)
+var updateInterval = setInterval(updateTime, 60000);
+
+function updateTime() {
+    var query = firebase.database().ref().orderByKey();
+    query.once("value")
+        .then(function (snapshot) {
+            snapshot.forEach(function (childSnapshot) {
+                var thisFirstTime = childSnapshot.val().firstTime;
+                var thisFrequency = childSnapshot.val().frequency;
+                // var thisFirstConverted = moment(thisFirstTime, "HH:mm").subtract(1, "years");
+                var thisDiffTime = moment().diff(moment(thisFirstTime, "HH:mm").subtract(1, "years"), "minutes");
+                // console.log(thisFirstTime, thisFrequency, thisDiffTime);
+                var thisRemainder = thisDiffTime % thisFrequency;
+                var newMinutes = thisFrequency - thisRemainder;
+                var newTime = moment().add(newMinutes, "minutes").format("hh:mm");
+                var key = childSnapshot.key;
+                database.ref(key).update({
+                    nextTrain: newTime,
+                    minutesAway: newMinutes,
+                });
+                var thisRow = childSnapshot.val().row;
+                var idNameNext = "next-train" + thisRow;
+                var idNameMinutes = "minutes-away" + thisRow;
+                $("#"+idNameNext).text(newTime);
+                $("#"+idNameMinutes).text(newMinutes);
+            });
+        });
+}
