6f4d380bde649f7ace76c2bc675cb645def43d7d
hilbo <hardimanhk@gmail.com>
2018-09-20 14:44:08 -0400

    adding google and github sign ins

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -170,6 +170,34 @@ function signIn() {
     });
 }
 
+function googleLogin() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithRedirect(provider);
+}
+
+function githubLogin() {
+    var provider = new firebase.auth.GithubAuthProvider();
+    firebase.auth().signInWithRedirect(provider);
+    // firebase.auth().getRedirectResult().then(function (result) {
+    //     if (result.credential) {
+    //         // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+    //         var token = result.credential.accessToken;
+    //         // ...
+    //     }
+    //     // The signed-in user info.
+    //     var user = result.user;
+    // }).catch(function (error) {
+    //     // Handle Errors here.
+    //     var errorCode = error.code;
+    //     var errorMessage = error.message;
+    //     // The email of the user's account used.
+    //     var email = error.email;
+    //     // The firebase.auth.AuthCredential type that was used.
+    //     var credential = error.credential;
+    //     // ...
+    // });
+}
+
 function logOut() {
     firebase.auth().signOut();
 }
@@ -180,3 +208,5 @@ $("#create-user").on("click", signInToggle);
 $("#user-create").on("click", createUser);
 $("#sign-in-page").on("click", signInToggle);
 $("#logout").on("click", logOut);
+$("#google-login").on("click", googleLogin);
+$("#github-login").on("click", githubLogin);
